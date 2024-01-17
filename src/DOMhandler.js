b0ee264b56038402e94d3c08c007f1915fe34597
dymjin <dymjin@gmail.com>
2024-01-17 13:44:02 +0200

    Edit DOMhandler* *add 2 new inputs, select(for priority select), date(for due-date) *simplified funcs + generalized func args

--- src/DOMhandler.js
+++ src/DOMhandler.js
@@ -5,29 +5,24 @@ const init = (() => {
     return { add, get };
 })();
 
-function addElement(name, text, type, parent = document.body) {
+function addElement(name, text, parent, type, data = false) {
+    type = type || 'div';
     const elem = document.createElement(type);
-    elem.className = name;
-    elem.textContent = text;
+    name ? elem.className = name : name = '';
+    text ? elem.textContent = text : text = '';
+    data ? elem.setAttribute('data', init.get()) : data = '';
     parent.appendChild(elem);
     return elem;
 }
 
-function addContainer(name, parent, type) {
-    type = type || 'div';
-    const container = addElement(name, '', type, parent);
-    container.setAttribute('data', init.get());
-    parent.appendChild(container);
-    return container;
-}
-
-function addInput(parent, content, placeholder, disabled = false, name) {
+function addInput(name, text, parent, placeholder, disabled, type) {
     const input = document.createElement('input');
-    input.setAttribute('type', 'text');
+    type = type || 'text';
+    input.setAttribute('type', type);
     placeholder ? input.setAttribute('placeholder', placeholder) : placeholder = '';
-    input.value = content;
+    text ? input.value = text : text = '';
     input.className = name;
-    if (disabled) input.disabled = true;
+    disabled ? input.disabled = true : disabled = false;
     parent.appendChild(input);
     return input;
 }
@@ -35,27 +30,32 @@ function addInput(parent, content, placeholder, disabled = false, name) {
 function addTodoDOM(title, desc) {
     title = title || '';
     desc = desc || '';
-    addContainer('todo-container', document.querySelector('.project-container'));
-    addInput(document.querySelector(`.todo-container[data="${init.get()}"]`), title, '', true, 'todo-title');
-    addInput(document.querySelector(`.todo-container[data="${init.get()}"]`), desc, '', true, 'todo-desc');
+    addElement('todo-container', '', document.querySelector('.project-container'), '', true);
+    addInput('todo-title', title, document.querySelector(`.todo-container[data="${init.get()}"]`), '', true);
+    addInput('todo-desc', desc, document.querySelector(`.todo-container[data="${init.get()}"]`), '', true);
     init.add();
 }
 
 function addModal() {
-    addElement('dialog-container', '', 'div', document.querySelector('.project-container'));
-    addElement('todo-dialog', '', 'dialog', document.querySelector('.dialog-container'));
-    addElement('dialog-form', '', 'form', document.querySelector('.todo-dialog'));
-    addInput(document.querySelector('.dialog-form'), '', '', false, 'todo-input-title');
-    addInput(document.querySelector('.dialog-form'), '', '', false, 'todo-input-desc');
-    addElement('todo-confirm', '+', 'button', document.querySelector('.dialog-form'));
+    const dialogContainer = addElement('dialog-container', '', document.querySelector('.project-container'), '', false);
+    const todoDialog = addElement('todo-dialog', '', dialogContainer, 'dialog', false);
+    const dialogForm = addElement('dialog-form', '', todoDialog, 'form', false);
+    addInput('todo-input-title', '', dialogForm, '', false);
+    addInput('todo-input-desc', '', dialogForm, '', false);
+    addInput('due-date', '', dialogForm, '', false, 'date');
+    const select = addElement('priority-select', '', dialogForm, 'select', false);
+    addElement('', 'Low', select, 'option', false);
+    addElement('', 'Medium', select, 'option', false);
+    addElement('', 'High', select, 'option', false);
+    addElement('todo-confirm', '+', dialogForm, 'button', false);
 }
 
 function addProjectDOM(title) {
     title = title || '';
-    addElement('project-container', '', 'div', document.querySelector('.page-container'));
+    addElement('project-container', '', document.querySelector('.page-container'), '', false);
     addModal();
-    addInput(document.querySelector('.project-container'), title, 'My project', false, '.project-title');
-    addElement('add-todo', '+', 'button', document.querySelector('.page-container'));
+    addInput('project-title', title, document.querySelector('.project-container'), 'Title', false);
+    addElement('add-todo', '+', document.querySelector('.page-container'), 'button', false);
 }
 
 export { addProjectDOM, addTodoDOM };

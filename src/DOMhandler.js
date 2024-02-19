fe7ed0c06a4b952344fec92c58b46d5501b1dac7
dymjin <dymjin@gmail.com>
2024-02-19 13:23:06 +0200

    Refactor DOMhandler code* *clearDOM now only works if both todoContainer + projectContainer exist *remove unnecessary validation from addElement *remove unnecessary validation + setting of type from addInput *remove unnecessary conditional + duplicate code from addCheckbox, rename DOM elements *add addTab func in place of projectTab func, creates new tab with args *addTodoTab func now creates new tab with an extra duedate + visual priority *both addProjectDOM + addTodoDOM now adds their respective tabs

--- src/DOMhandler.js
+++ src/DOMhandler.js
@@ -1,15 +1,17 @@
 import styles from './style.css';
 import { format } from "date-fns";
 
-function clearDOM(element) {
+function clearDOM(mode) {
     const projectContainer = document.querySelector('.project-container');
     const todoContainer = document.querySelector('.todo-container');
-    if (element === "project") {
-        document.querySelector('.page-container')
-            .removeChild(projectContainer);
-    }
-    if (element === "todo") {
-        projectContainer.removeChild(todoContainer);
+    if (projectContainer && todoContainer) {
+        if (mode === "project") {
+            document.querySelector('.page-container')
+                .removeChild(projectContainer);
+        }
+        if (mode === "todo") {
+            projectContainer.removeChild(todoContainer);
+        }
     }
 }
 
@@ -29,33 +31,28 @@ function clearTabDOM(tab) {
     }
 }
 
-function addElement(name, text, parent, type) {
-    type = type || 'div';
+function addElement(name = '', text = '', parent, type = 'div') {
     const elem = document.createElement(type);
-    name ? elem.className = name : name = '';
-    text ? elem.textContent = text : text = '';
+    elem.className = name;
+    elem.textContent = text;
     parent.appendChild(elem);
     return elem;
 }
 
-function addInput(name, text, parent, placeholder, type) {
-    type = type || 'text';
-    const input = document.createElement('input');
-    input.setAttribute('type', type);
-    placeholder ? input.setAttribute('placeholder', placeholder) : placeholder = '';
-    text ? input.value = text : text = '';
-    input.className = name;
-    parent.appendChild(input);
+function addInput(name = '', text = '', parent, type = 'text', placeholder) {
+    const input = addElement(name, '', parent, 'input');
+    input.type = type;
+    input.placeholder = placeholder;
+    input.value = text;
     return input;
 }
 
-function addSelect(name, parent, priority) {
-    priority = priority || '';
+function addSelect(name = '', parent, priority = '') {
     const select = addElement(name, '', parent, 'select');
     const defaultOption = addElement('option', 'Set priority', select, 'option');
-    defaultOption.setAttribute('disabled', true);
-    defaultOption.setAttribute('hidden', true);
-    defaultOption.setAttribute('value', '');
+    defaultOption.disabled = true;
+    defaultOption.hidden = true;
+    defaultOption.value = '';
     addElement('option', 'Low', select, 'option');
     addElement('option', 'Medium', select, 'option');
     addElement('option', 'High', select, 'option');
@@ -71,77 +68,68 @@ function addSelect(name, parent, priority) {
     return select;
 }
 
-function addChecklist(parent, checkbox) {
-    if (!checkbox) {
-        const label = addElement('', '', parent, 'label');
-        addInput('checklist-item', '', label, '', 'checkbox');
-        addInput('checklist-item-name', '', label, 'item name');
-        return label;
-    } else {
-        const label = addElement('checklist-item-container', '', parent, 'label');
-        const checkboxInput = addInput('checklist-item', '', label, '', 'checkbox');
-        checkboxInput.checked = checkbox.state;
-        addInput('checklist-item-title', checkbox.title, label, 'item name');
-        return label;
+function addCheckbox(parent, checkbox) {
+    const label = addElement('checkbox-item-container', '', parent, 'label');
+    const checkboxTick = addInput('checkbox-tick', '', label, 'checkbox');
+    const checkboxTitle = addInput('checkbox-title', '', label, '', 'Item name');
+    if (checkbox) {
+        checkboxTick.checked = checkbox.state;
+        checkboxTitle.value = checkbox.title;
     }
+    return label;
 }
 
-function addProjectTabs(text, id) {
-    const projectTab = addElement('project', '', document.querySelector('.projectlist-container'));
-    const projectTabTitle = addInput('project-tab-title', text, projectTab, 'My project')
-    const removeProjectTab = addElement('remove-project-tab', 'remove', projectTab, 'button');
-    const elements = [projectTab, projectTabTitle, removeProjectTab];
+function addTab(name, text, parent, projectID, todoID) {
+    const tabContainer = addElement(name, '', parent);
+    const tabtTitle = addInput(`${name}-tab-title`, text, tabContainer, '', 'My project');
+    tabtTitle.disabled = true;
+    const removeTab = addElement(`${name}-remove-tab`, 'remove', tabContainer, 'button');
+    const elements = [tabContainer, tabtTitle, removeTab];
     elements.forEach(elem => {
-        elem.setAttribute('data', id);
-    })
+        if (todoID) { elem.setAttribute('data', `${projectID}-${todoID}`); }
+        else { elem.setAttribute('data', projectID); }
+    });
     return elements;
 }
 
-function addTodoCards(text, dueDate, todoID, priority = 'Low', projectID) {
-    text = text || 'My todo';
-    priority ? 0 : priority = 'Low';
-    dueDate = dueDate || format(new Date(), "dd-MM-yyyy")
-    let currentProject = JSON.parse(localStorage.getItem('current_project'));
-    const todoCard = addElement('todo', '', document.querySelector(`.project[data="${currentProject.id}"]`));
+function addTodoTab(text = 'My todo', dueDate = format(new Date(), "dd-MM-yyyy"), priority = 'Low', projectID, todoID) {
+    const todoTab = addTab('todo', text, document.querySelector(`.project[data="${projectID}"]`), projectID, todoID);
+    const todoTabDueDate = addElement('todo-tab-duedate', dueDate, todoTab[0]);
+    todoTabDueDate.setAttribute('data', `${projectID}-${todoID}`)
+    todoTab[0].insertBefore(todoTabDueDate, todoTab[2])
     switch (priority) {
         case 'Low':
-            todoCard.style.backgroundColor = '#85ffc8';
+            todoTab[0].style.backgroundColor = '#85ffc8';
             break;
         case 'Medium':
-            todoCard.style.backgroundColor = '#fcbf5d';
+            todoTab[0].style.backgroundColor = '#fcbf5d';
             break;
         case 'High':
-            todoCard.style.backgroundColor = '#fc5151';
+            todoTab[0].style.backgroundColor = '#fc5151';
             break;
     }
-    const todoTabTitle = addElement('todo-card-title', text, todoCard);
-    const todoDueDate = addElement('todo-card-duedate', dueDate, todoCard);
-    const removeTodoTab = addElement('remove-todo-card', 'remove', todoCard, 'button');
-    const elements = [todoCard, todoTabTitle, removeTodoTab, todoDueDate];
-
-    elements.forEach(elem => {
-        elem.setAttribute('data', `${projectID}-${todoID}`);
-    })
-    return elements;
+    return todoTab;
 }
 
-function addTodoDOM(title, desc, dueDate, priority, notes, checkboxArr) {
+function addTodoDOM(title, desc, dueDate, priority, notes, projectID, todoID) {
     clearDOM('todo');
     const todoContainer = addElement('todo-container', '', document.querySelector('.project-container'));
     const todoTitle = addInput('todo-title', title, todoContainer, 'Title');
     const todoDesc = addInput('todo-desc', desc, todoContainer, 'Description');
-    const todoDueDate = addInput('due-date', dueDate, todoContainer, '', 'date');
+    const todoDueDate = addInput('due-date', dueDate, todoContainer, 'date');
     const todoSelect = addSelect('todo-select', todoContainer, priority);
     const todoNotes = addElement('todo-notes', notes, todoContainer, 'textarea');
-    todoNotes.setAttribute('placeholder', 'Add note')
+    todoNotes.placeholder = 'Add note';
     const addChecklistBtn = addElement('add-checklist', 'add checklist', todoContainer, 'button');
     addElement('checklist-container', '', todoContainer);
+    addTodoTab(title, dueDate, priority, projectID, todoID);
     return [todoTitle, todoDesc, todoDueDate, todoSelect, todoNotes, addChecklistBtn]
 }
 
-function addProjectDOM() {
+function addProjectDOM(text, id) {
     clearDOM('project');
     addElement('project-container', '', document.querySelector('.page-container'));
+    addTab('project', text, document.querySelector('.projectlist-container'), id);
 }
 
-export { addProjectDOM, addTodoDOM, clearDOM, clearTabDOM, addChecklist, addProjectTabs, addTodoCards };
+export { addProjectDOM, addTodoDOM, clearDOM, clearTabDOM, addCheckbox };
